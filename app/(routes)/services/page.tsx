cd25f12be1a0a38083a406c23bee9549504ee33a
JorgeAd99 <jorgeenriqueadorno@gmail.com>
2024-08-21 09:51:47 -0300

    changes 2

--- app/(routes)/services/page.tsx
+++ app/(routes)/services/page.tsx
@@ -9,7 +9,6 @@ const ServicesPage= () => {
         <>
             <TransitionPage />
             <CircleImage />
-            <AvatarServices />
             <div className="grid items-center justify-center h-screen max-w-5xl gap-6 mx-auto md:grid-cols-2">
                 <div className="max-w-[450px]">
 
